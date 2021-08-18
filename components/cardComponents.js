3ed9dad639276fbeeb938978b1f792ac486227fd
jeison <jeison.com>
2021-08-18 10:13:29 -0500

    new components grid

--- components/cardComponents.js
+++ components/cardComponents.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function CardComponents(){
+    const [isHover1,setIsHover1]=useState(false)
+    const [isHover2,setIsHover2]=useState(false)
+    const [isHover3,setIsHover3]=useState(false)
+    const [isHover4,setIsHover4]=useState(false)
     return(
         <div>
             <h3 className="text-center ">FEATURED PRODUCTS</h3>
             <h2 className="text-center mb-4"><b >------------------------------</b></h2>
-            <div className="container center position-relative mt-2 mb-2">
-                <div className="row">
-                    <div className="col-4 text-center mb-5 ">
-                        <div className="row">
-                            <div className="card" id="imageHover" style={{width: "18rem"}}>
-                                <img src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product1_medium.jpg?v=1568108096" className="card-img-top" alt="image"/>
+            <div className="container center mt-2  mx-5 position-">
+                <div className="row ">
+                    <div className="col-3 text-center mb-5  " >
+                        <div className="row" >
+                            <div className="card  position-relative " onMouseEnter={()=>setIsHover1(true)} onMouseLeave={()=>setIsHover1(false)} id="imageHover" style={{width: "18rem",left:"50%"}}>
+                                {!isHover1&&(<img id="imageHoverImg"  src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product1_medium.jpg " className="card-img-top position-absolute" style={{width:"90%",zIndex:"99"}} alt="image"/>
+                                )}
+                                <img id="imageHoverImg" src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product32_9b1e0e9a-d130-4c20-b1ef-2ae3183d314d_medium.jpg" className="position-relative"  alt="image"/>
+
                                 <div className="card-body">
                                     <h5 className="card-title ">REDUCERS & ADAPTERS</h5>
                                     <p className="card-text">
@@ -19,16 +26,18 @@ export default function CardComponents(){
                                 <ul className="list-group list-group-flush">
                                     Rs. 100.00 Rs. 500.00
                                 </ul>
-                                <div className="card-body" id="ClassHover">
-                                    <button className="btn opacity-100 text-white" >ADD TO CARD</button>
+                                <div className="card-body " id="ClassHover">
+                                    <button className="btn rounded-5 opacity-100 text-white" >ADD TO CARD</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-4 text-center ">
+                    <div className="col-3 text-center ">
                         <div className="row">
-                            <div className="card" id="imageHover" style={{width: "18rem"}}>
-                                <img src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product1_medium.jpg?v=1568108096" className="card-img-top" alt="image"/>
+                            <div className="card  " onMouseEnter={()=>setIsHover2(true)} onMouseLeave={()=>setIsHover2(false)} id="imageHover" style={{width: "18rem",left:"50%"}}>
+                                {!isHover2&&(<img id="imageHoverImg"  src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product18_70da4d32-c08f-47b9-9bf1-f0ec27b87dc0_medium.jpg" className="card-img-top position-absolute" style={{width:"90%",zIndex:"99"}} alt="image"/>
+                                )}
+                                <img id="imageHoverImg" src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product19_medium.jpg" className="position-relative"  alt="image"/>
                                 <div className="card-body">
                                     <h5 className="card-title ">THE VISE</h5>
                                     <p className="card-text">
@@ -44,17 +53,39 @@ export default function CardComponents(){
                         </div>
 
                     </div>
-                    <div className="col-4 text-center">
+                    <div className="col-3 text-center">
                         <div className="row">
-                            <div className="card" id="imageHover" style={{width: "18rem"}}>
-                                <img src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product1_medium.jpg?v=1568108096" className="card-img-top" alt="image"/>
+                            <div className="card  " onMouseEnter={()=>setIsHover3(true)} onMouseLeave={()=>setIsHover3(false)} id="imageHover" style={{width: "18rem",left:"50%"}}>
+                                {!isHover3&&(<img id="imageHoverImg"  src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product12_31bd55cf-6146-46ae-a11c-8fc2f076edde_medium.jpg" className="card-img-top position-absolute" style={{width:"90%",zIndex:"99"}} alt="image"/>
+                                )}
+                                <img id="imageHoverImg" src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product22_medium.jpg" className="position-relative"  alt="image"/>
                                 <div className="card-body">
-                                    <h5 className="card-title ">REDUCERS & ADAPTERS</h5>
+                                    <h5 className="card-title ">HACKSAW BLADES KIT</h5>
                                     <p className="card-text">
                                         Nam tempus turpis at metus scelerisque placerat....</p>
                                 </div>
                                 <ul className="list-group list-group-flush">
-                                    Rs. 100.00 Rs. 500.00
+                                    Rs. 389.00
+                                </ul>
+                                <div className="card-body" id="ClassHover">
+                                    <button className="btn opacity-100 text-white" >ADD TO CARD</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3 text-center">
+                        <div className="row">
+                            <div className="card  " onMouseEnter={()=>setIsHover4(true)} onMouseLeave={()=>setIsHover4(false)} id="imageHover" style={{width: "18rem",left:"50%"}}>
+                                {!isHover4&&(<img id="imageHoverImg"  src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product28_b6beb2f3-9b6f-4918-9529-6a27a19ecfc4_medium.jpg" className="card-img-top position-absolute" style={{width:"90%",zIndex:"99"}} alt="image"/>
+                                )}
+                                <img id="imageHoverImg" src="https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product23_medium.jpg" className="position-relative"  alt="image"/>
+                                <div className="card-body">
+                                    <h5 className="card-title ">MONKEY SPANNERS</h5>
+                                    <p className="card-text">
+                                        Nam tempus turpis at metus scelerisque placerat....</p>
+                                </div>
+                                <ul className="list-group list-group-flush">
+                                    Rs. 199.00
                                 </ul>
                                 <div className="card-body" id="ClassHover">
                                     <button className="btn opacity-100 text-white" >ADD TO CARD</button>
@@ -66,20 +97,21 @@ export default function CardComponents(){
             </div>
             <style jsx global>
                 {`
-                .clasehover:hover{
-                background-color: black !important;
-                }
+                
                 #ClassHover:hover{
                 background-color: black !important;
                 }
-                #imageHover:hover{
-                box-sizing:border-box;
-                display:block;
-                 /*Padding igual al ancho de la imagen nueva*/
-                background: url("https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product32_9b1e0e9a-d130-4c20-b1ef-2ae3183d314d_medium.jpg")
+                #imageHoverImg {
+                
+                background-image: url("https://cdn.shopify.com/s/files/1/0179/0453/3558/products/product32_9b1e0e9a-d130-4c20-b1ef-2ae3183d314d_medium.jpg")
 
                 }
+                #imageHover:hover{
+                opacity: 100%;
+                }
 `}
+
+
             </style>
         </div>
     )
--- components/gridComponent.js
+++ components/gridComponent.js
@@ -1,45 +1,74 @@
+import Image from 'next/image'
+import profilePic from '../public/bolsa.png'
+import React from "react";
+
 export default function GridComponent(){
 
     return(
         <>
             <div className="container justify-content-center">
-                <div className="row">
-                    <div className="col-sm-3 bg-primary mx-2" id="imageGrid">
-                        <div className=""  style={{width: "18rem"}}>
-                            <div className="car">
-                                <h5 className="title">Card title</h5>
-                                <p className="">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+                <div className="row" style={{marginLeft:"10%"}}>
+                    <div className="col-sm-3 bg-primary mx-2 position-relative" id="imageGrid">
+                        <div className=""  >
+                            <div className="position-absolute"id="imageGrid" style={{padding:"10%",top:"50%",left:"0",right:"0",bottom:"0"}}>
+                                <Image className="shadow" src={profilePic} alt="imagen publicidad"/>
+                            </div>
+                            <div className="align-content-center "style={{margin:"20%",left:"10%"}}>
+                                <h2 className="title" >
+                                    {/*<p style=" border-bottom-color:#FF0000; border-bottom-style:dashed; border-bottom-width:2px; border-top-color:#000099; border-top-style:solid; border-top-width:1px;"> Un ejemplo de estilos de bordes </p>*/}
+                                    Asset Tools
+                                </h2>
+                                <h6  style={{width: "94%",backgroundColor:"black"}} className="text-white p-2" >FLAT 35% DISCOUNT</h6>
+
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-7  ">
                         <div className="row">
-                            <div className="col-8 col-sm-6 bg-success " id="imageGrid">
+                            <div className="col-8 position-relative col-sm-6 bg-success  " id="imageGrid">
                                 <div className=""  style={{width: "18rem"}}>
-                                    <div className="car">
-                                        <h5 className="title">Card title</h5>
-                                        <h6 className="subtitle mb-2 text-muted">Card subtitle</h6>
-                                        <p className="text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años.</p>
+                                    <div className="position-absolute"id="imageGrid" style={{paddingInlineStart:"50%",top:"0"}}>
+                                        <img style={{width: "100%"}} src="https://cdn.shopify.com/s/files/1/0179/0453/3558/files/img1_400x.png"/>
+                                    </div>
+                                    <div className="car"style={{marginTop:"50%",left:"0"}}>
+                                        <p className="text">Modern Tool</p>
+                                        <h4 className="title">
+                                            Suprimo’s Kit
+                                            </h4>
+                                        <h6  style={{width: "65%"}} className="bg-warning  p-2" >COUPON CODE: 2134</h6>
+
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-4 col-sm-5 bg-danger mx-2 " id="imageGrid">
+                            <div className="col-4 col-sm-5 bg-danger mx-2 position-relative" id="imageGrid">
                                 <div className=""  style={{width: "18rem"}}>
-                                    <div className="car">
-                                        <h5 className="title">Card title</h5>
-                                        <h6 className="subtitle mb-2 text-muted">Card subtitle</h6>
-                                        <p className="card-text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años. </p>
+                                    <div className="position-absolute"id="imageGrid" style={{paddingInlineStart:"40%",top:"0"}}>
+                                        <img style={{width: "100%"}} src="https://ftmapp-production.s3.amazonaws.com/uploads/product/picture/1756/large_retina_GWS22230.png" alt="hola"/>
+                                    </div>
+                                    <div className="car"style={{marginTop:"50%",left:"0"}}>
+                                        <p className="text">Modern Tool</p>
+                                        <h4 className="title">
+                                            Suprimo’s Kit
+                                        </h4>
+                                        <h6  style={{width: "65%"}} className="bg-warning  p-2" >COUPON CODE: 2134</h6>
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="row ">
-                            <div className="col-12 col-sm-11 bg-warning mt-2" id="imageGrid">
+                            <div className="col-12 col-sm-11 bg-warning mt-2 position-relative" id="imageGrid">
                                 <div className=""  >
-                                    <div className="car">
-                                        <h5 className="title">Card title</h5>
-                                        <h6 className="subtitle mb-2 text-muted">Card subtitle</h6>
-                                        <p className="text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas  las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+                                    <div className="position-absolute"id="imageGrid" style={{paddingInlineStart:"60%",top:"0"}}>
+                                        <img style={{width: "100%"}} src="https://ftmapp-production.s3.amazonaws.com/uploads/product/picture/1756/large_retina_GWS22230.png" alt="hola"/>
+                                    </div>
+                                    <div className="align-content-center "style={{margin:"20%",left:"10%"}}>
+                                        <h2 className="title" >
+                                            {/*<p style=" border-bottom-color:#FF0000; border-bottom-style:dashed; border-bottom-width:2px; border-top-color:#000099; border-top-style:solid; border-top-width:1px;"> Un ejemplo de estilos de bordes </p>*/}
+                                            <b>Flat 65% Off*</b> on Hand Tools
+                                        </h2>
+                                        <h6  style={{width: "85%",borderStyle:"dashed"}} className="bg-warning  p-2 text-danger" >COUPON CODE: 2134</h6>
+
                                     </div>
                                 </div>
                             </div>
@@ -52,9 +81,14 @@ export default function GridComponent(){
                     
                     position: center
                     }
+                    
                     #imageGrid:hover{
-                    background: url("https://i.pinimg.com/originals/2a/70/dd/2a70ddeb11454102b58ed887899868c0.png");
-                    }                    
+                    padding-inline-start:10%;
+                    top:10%;
+                    transition: 2s;
+                  //  background: url("");
+                    }   
+                                    
 `}
                 </style>
             </div>
